963e56cb9f4c1d322d5e458597ae727c8c7776db
Digory <digory1@googlemail.com>
2018-07-17 17:09:55 +0100

    All extensions finished

--- models/game.rb
+++ models/game.rb
@@ -6,19 +6,6 @@ class Game
     @player_choices_hash = {"player_1" => @player_1_input, "player_2" => @player_2_input}
   end
 
-  # def outcome()
-  #   return "draw" if @player_1_input == @player_2_input
-  #     if (@player_1_input == "scissors" || @player_2_input == "scissors") && (@player_1_input == "paper" || @player_2_input == "paper")
-  #       return "scissors wins"
-  #     end
-  #   end
-  # def outcome()
-  #   return "draw" if @array[0] == @array[1]
-  #   return "scissors wins" if @array.include?("scissors") && @array.include?("paper")
-  #   return "rock wins" if @array.include?("rock") && @array.include?("scissors")
-  #   return "paper wins" if @array.include?("paper") && @array.include?("rock")
-  # end
-
   def outcome()
     return "draw" if @player_choices_hash["player_1"] == @player_choices_hash["player_2"]
     if @player_choices_hash.has_value?("scissors") && @player_choices_hash.has_value?("paper")
